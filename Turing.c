a5187a049e76a2bbfd5db11f47bf7c810d9b4bb6
Hendy Rodrigues <mr.syslogg@gmail.com>
2016-10-26 03:19:28 -0300

    Add move head and Read/Write in tape

--- Turing.c
+++ Turing.c
@@ -2,7 +2,8 @@
 #include <stdlib.h>
 #include "Turing.h"
 
-#define TAM_MAX 5000
+#define TM_MAX 5000
+#define BLANK 2
 
 struct machineturing {
 	int tape[TAM_MAX];
@@ -13,6 +14,26 @@ TuringMachine * Create() {
 	TuringMachine * tm = (TuringMachine *) malloc(sizeof(TuringMachine));
 	tm->head = TAM_MAX/2;
 	
+	int i;
+	for(i = 0; i < TAM_MAX; i++) {
+		tm->tape[i] = BLANK;
+	}
+	
 	return tm;
 }
 
+void MoveHeadLeft (TuringMachine *  tm) {
+	tm->head--;
+}
+
+void MoveHeadRight (TuringMachine *  tm) {
+	tm->head++;
+}
+
+int Read(TuringMachine * tm) {
+	return tm->tape[tm->head];
+}
+
+void Write(TuringMachine * tm, int value) {
+	tm->tape[tm->head] = (value != 1 && value != 0 && value != BLANK ? tm->tape[tm->head] : value);
+}
--- Turing.h
+++ Turing.h
@@ -3,3 +3,10 @@ typedef struct machineturing TuringMachine;
 
 //Command
 TuringMachine * Create();
+
+//Moving
+void MoveHeadLeft (TuringMachine *  tm);
+void MoveHeadRight (TuringMachine *  tm);
+
+//Operation
+int Read(TuringMachine * tm);
